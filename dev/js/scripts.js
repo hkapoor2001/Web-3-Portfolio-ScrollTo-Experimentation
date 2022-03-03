e91ec7049c5927f81c7ba4071fd1bc894337a7ee
hkapoor2001 <hkapoor2001@gmail.com>
2022-03-03 14:28:16 -0500

    It works

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,11 +5,13 @@ import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 
+
 let sections = gsap.utils.toArray(".stage");
 let scrollContainer = document.querySelector("#hscroll");
 
 ScrollTrigger.matchMedia({
   "(min-width: 992px)": function() {
+
     let scrollTween = gsap.to(sections, {
       xPercent: -100 * (sections.length - 1),
       ease: "none"
@@ -22,14 +24,9 @@ ScrollTrigger.matchMedia({
       scrub: 1,
       end: () => "+=" + scrollContainer.offsetWidth,
     });
-
-    
   },
-  "all": function() {
-    
-  }
+  "all": function() {}
 });
-
 
 
 const wideViewport = window.matchMedia("(min-width: 992px)");
@@ -43,15 +40,12 @@ document.querySelectorAll("#navmenu a").forEach(element => {
     
     const targetElement = document.getElementById(id)
     const navBar = document.getElementById('masthead')
-    
-    
+
     if(wideViewport.matches) {
       gsap.to(window, {
-        scrollTo: ( (targetElement.offsetLeft)) * ( scrollContainer.offsetWidth / (scrollContainer.offsetWidth - targetElement.offsetWidth)),
-        // scrollTo: ( (targetElement.offsetLeft + navBar.offsetWidth*sections.indexOf(targetElement)) * ( scrollContainer.offsetWidth / (scrollContainer.offsetWidth - targetElement.offsetWidth)) ),
+        scrollTo: ( (targetElement.offsetLeft) * ( scrollContainer.offsetWidth / (scrollContainer.offsetWidth - targetElement.offsetWidth)) ),
         duration: 2
       })
-      console.log("scrolltriggerisactive")
     } else {
       gsap.to(window, {
         scrollTo: targetElement,
@@ -62,3 +56,63 @@ document.querySelectorAll("#navmenu a").forEach(element => {
   });
   
 });
+
+
+
+// let sections = gsap.utils.toArray(".stage");
+// let scrollContainer = document.querySelector("#hscroll");
+
+// ScrollTrigger.matchMedia({
+//   "(min-width: 992px)": function() {
+//     let scrollTween = gsap.to(sections, {
+//       xPercent: -100 * (sections.length - 1),
+//       ease: "none"
+//     });
+
+//     let horizontalScroll = ScrollTrigger.create({
+//       animation: scrollTween,
+//       trigger: scrollContainer,
+//       pin: true,
+//       scrub: 1,
+//       end: () => "+=" + scrollContainer.offsetWidth,
+//     });
+
+    
+//   },
+//   "all": function() {
+    
+//   }
+// });
+
+
+
+// const wideViewport = window.matchMedia("(min-width: 992px)");
+
+// document.querySelectorAll("#navmenu a").forEach(element => {
+  
+//   element.addEventListener('click', function(e) {
+    
+//     e.preventDefault();
+//     const id = this.getAttribute('href').split('#')[1];
+    
+//     const targetElement = document.getElementById(id)
+//     const navBar = document.getElementById('masthead')
+    
+    
+//     if(wideViewport.matches) {
+//       gsap.to(window, {
+//         scrollTo: ( (targetElement.offsetLeft)) * ( scrollContainer.offsetWidth / (scrollContainer.offsetWidth - targetElement.offsetWidth)),
+//         // scrollTo: ( (targetElement.offsetLeft + navBar.offsetWidth*sections.indexOf(targetElement)) * ( scrollContainer.offsetWidth / (scrollContainer.offsetWidth - targetElement.offsetWidth)) ),
+//         duration: 2
+//       })
+//       console.log("scrolltriggerisactive")
+//     } else {
+//       gsap.to(window, {
+//         scrollTo: targetElement,
+//         duration: 2
+//       })
+//     }
+    
+//   });
+  
+// });
